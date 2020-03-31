41759eb08111cf3f4c7b187be184a8daa7086836
Raman But-Husaim <raman_but-husaim@live.com>
2020-03-31 23:13:52 +0300

    Add APIGatewayEvents package and slightly modify handler

--- src/QuLaBot/Function.cs
+++ src/QuLaBot/Function.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -12,16 +8,20 @@ namespace QuLaBot
 {
     public class Function
     {
-        
+
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest apigProxyEvent)
         {
-            return input?.ToUpper();
+            return new APIGatewayProxyResponse
+            {
+                Body = apigProxyEvent.Body,
+                StatusCode = 200,
+            };
         }
     }
 }
